15f7e52b85de045c81e5d4d90d5995b6ad20c255
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-02 17:15:02 -0500

    Add second excersise

--- src/first-excersises.clj
+++ src/first-excersises.clj
@@ -1,9 +1,18 @@
 ;; Use the str, vector, list, hash-map, and hash-set functions.
-("This is a string")                                        ; => STRING
+"This is a string!"                         ; => STRING
 [1 "hello" "bye"]                                           ; => VECTOR
 '("hello again" "by again")                                 ; => LIST
 {:name "Daniel" :age 26}                                    ;=> hash-map
 #{"apple" "banana" "cherry"}                                ;=> hash-set
+
+;; Write a function that takes a number and adds 100 to it.
+
+(defn addition
+  [num]
+  (+ num 100))
+
+;
+
 
 
 
