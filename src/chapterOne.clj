3d5d9ed18c693cf793135c9c58b4aaca61ae9da1
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-01 20:48:01 -0500

    Add boolean, str and simple operators

--- src/chapterOne.clj
+++ src/chapterOne.clj
@@ -1,2 +1,16 @@
 (defn foo [x]
   (println (+ x x)))
+
+(+ 1 2 3)
+; => 6
+
+(str "It was the panda " "in the library " "with a dust buster")
+; => "It was the panda in the library with a dust buster"
+
+(if true
+  "By Zeus's hammer!"
+  "By Aquaman's trident!")
+
+(if false
+  "By Zeus's hammer!"
+  "By Aquaman's trident!")
