41cbb2057c641be8b854b312d408c6dcf6858a17
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-01 23:31:11 -0500

    Add SET

--- src/chapterOne.clj
+++ src/chapterOne.clj
@@ -137,3 +137,37 @@
 
 (conj '(1 2 3) 4)
 ; => (4 1 2 3)
+
+;;SETS
+
+#{"kurt vonnegut" 20 :icicle}
+
+(hash-set 1 1 2 2)
+; => #{1 2}
+
+(conj #{:a :b} :b)
+; => #{:a :b}
+
+(set [3 3 3 4 4])
+; => #{3 4}
+
+(contains? #{:a :b} :a)
+; => true
+
+(contains? #{:a :b} 3)
+; => false
+
+(contains? #{nil} nil)
+; => true
+
+(:a #{:a :b})
+; => :a
+
+(get #{:a :b} :a)
+; => :a
+
+(get #{:a nil} nil)
+; => nil
+
+(get #{:a :b} "kurt vonnegut")
+; => nil
