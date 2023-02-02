67a91e1222d3bf15ae75cb61257777d74eeac96e
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-02 08:35:12 -0500

    Add Destructuring

--- src/chapterOne.clj
+++ src/chapterOne.clj
@@ -259,4 +259,52 @@
 (favorite-things "Doreen" "gum" "shoes" "kara-te")
 ; => "Hi, Doreen, here are my favorite things: gum, shoes, kara-te"
 
+;DESTRUCTURING
 
+; This is not related to DESTRUCTURING, I WAS JUST PRACTICING
+
+(defn falsy [bool]
+       (if (= bool false)
+         (do (println "The function works, so you obtain:")
+             bool)
+         "You can't do this, it's a falsy function"))
+
+;This is related to DESTRUCTURING
+
+;; Return the first element of a collection
+(defn my-first
+  [[first-thing]] ; Notice that first-thing is within a vector
+  first-thing)
+
+(my-first ["oven" "bike" "war-axe"])
+; => "oven"
+
+(defn chooser
+  [[first-choice second-choice & unimportant-choices]]
+  (println (str "Your first choice is: " first-choice))
+  (println (str "Your second choice is: " second-choice))
+  (println (str "We're ignoring the rest of your choices. "
+                "Here they are in case you need to cry over them: "
+                (clojure.string/join ", " unimportant-choices))))
+
+(chooser ["Marmalade", "Handsome Jack", "Pigpen", "Aquaman"])
+; => Your first choice is: Marmalade
+; => Your second choice is: Handsome Jack
+; => We're ignoring the rest of your choices. Here they are in case \ you need to cry over them: Pigpen, Aquaman
+
+(defn announce-treasure-location
+   [{lat :lat lng :lng}]
+  (println (str "Treasure lat: " lat))
+  (println (str "Treasure lng: " lng)))
+
+(announce-treasure-location {:lat 28.22 :lng 81.33})
+; => Treasure lat: 28.22
+; => Treasure lng: 81.33
+
+(defn receive-treasure-location
+  [{:keys [lat lng] :as treasure-location}]
+  (println (str "Treasure lat: " lat))
+  (println (str "Treasure lng: " lng))
+
+  ;; One would assume that this would put in new coordinates for your ship
+  (println treasure-location))
