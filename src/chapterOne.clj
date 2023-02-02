2909b75ea079155b9369b4f7d82f72b3d5d70054
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-02 08:39:35 -0500

    Add Function Body

--- src/chapterOne.clj
+++ src/chapterOne.clj
@@ -308,3 +308,26 @@
 
   ;; One would assume that this would put in new coordinates for your ship
   (println treasure-location))
+
+;; FUNCTION BODY
+
+(defn illustrative-function
+  []
+  (+ 1 304)
+  30
+  "joe")
+
+(illustrative-function)
+; => "joe"
+
+(defn number-comment
+  [x]
+  (if (> x 6)
+    "Oh my gosh! What a big number!"
+    "That number's OK, I guess"))
+
+(number-comment 5)
+; => "That number's OK, I guess"
+
+(number-comment 7)
+; => "Oh my gosh! What a big number!"
