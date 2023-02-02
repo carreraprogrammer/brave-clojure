f9c960b396c97c5b6d5c326deb9b9f0fa4a829ea
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-01 21:09:29 -0500

    Add do operator

--- src/chapterOne.clj
+++ src/chapterOne.clj
@@ -14,3 +14,11 @@
 (if false
   "By Zeus's hammer!"
   "By Aquaman's trident!")
+
+(if true
+  (do (println "Success!")
+      "By Zeus's hammer!")
+  (do (println "Failure!")
+      "By Aquaman's trident!"))
+; => Success!
+; => "By Zeus's hammer!"
