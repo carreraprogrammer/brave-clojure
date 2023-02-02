b60a4e3a9853efd369bd983461757a199880b5f6
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-02 17:08:24 -0500

    First excersise

---
+++ src/first-excersises.clj
@@ -0,0 +1,9 @@
+;; Use the str, vector, list, hash-map, and hash-set functions.
+("This is a string")                                        ; => STRING
+[1 "hello" "bye"]                                           ; => VECTOR
+'("hello again" "by again")                                 ; => LIST
+{:name "Daniel" :age 26}                                    ;=> hash-map
+#{"apple" "banana" "cherry"}                                ;=> hash-set
+
+
+
