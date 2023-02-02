420a8b1d86b6b080fd2053fd096bf3098ba83495
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-02 10:49:43 -0500

    Understanding loop

--- src/chapterOne.clj
+++ src/chapterOne.clj
@@ -427,6 +427,20 @@
 
 ; => [:a]
 
+;; LOOP
+
+(loop [iteration 0]
+  (println (str "Iteration " iteration))
+  (if (> iteration 3)
+    (println "Goodbye!")
+    (recur (inc iteration))))
+; => Iteration 0
+; => Iteration 1
+; => Iteration 2
+; => Iteration 3
+; => Iteration 4
+; => Goodbye!
+
 
 
 
