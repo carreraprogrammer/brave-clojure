884e7c51669e1a834706551813f9513973faa838
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-02 11:26:14 -0500

    Explain regex

--- src/chapterOne.clj
+++ src/chapterOne.clj
@@ -442,5 +442,31 @@
 ; => Goodbye!
 
 
+;; REGEX
 
+(re-find #"^left-" "left-eye")
+; => "left-"
 
+(re-find #"^left-" "cleft-chin")
+; => nil
+
+(re-find #"^left-" "wongleblart")
+; => nil
+
+(def some-part {:name "left-arm" :size 3} )
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+(matching-part {:name "left-eye" :size 1})
+; => {:name "right-eye" :size 1}]
+
+(matching-part {:name "head" :size 3})
+; => {:name "head" :size 3}]
+
+(def string-part "left-arm")
+
+(defn change-string
+  [part]
+  (clojure.string/replace part #"^left" "right"))
