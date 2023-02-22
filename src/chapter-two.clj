51ae5541297bb8adaa4046b0d48a01d46091df71
carreraprogrammer <carreraprogrammer@gmail.com>
2023-02-22 09:01:55 -0500

    Add partial

--- src/chapter-two.clj
+++ src/chapter-two.clj
@@ -258,3 +258,37 @@
 
 (my-into [0] [1 2 3])
 ; => [0 1 2 3]
+
+;PARTIAL
+
+(def add10 (partial + 10))
+(add10 3)
+; => 13
+(add10 5)
+; => 15
+
+(def add-missing-elements
+  (partial conj ["water" "earth" "air"]))
+
+(add-missing-elements "unobtainium" "adamantium")
+; => ["water" "earth" "air" "unobtainium" "adamantium"
+
+(defn my-partial
+  [partialized-fn & args]
+  (fn [& more-args]
+    (apply partialized-fn (into args more-args))))
+
+(def add20 (my-partial + 20))
+(add20 3)
+; => 23
+
+(defn lousy-logger
+  [log-level message]
+  (condp = log-level
+    :warn (clojure.string/lower-case message)
+    :emergency (clojure.string/upper-case message)))
+
+(def warn (partial lousy-logger :warn))
+
+(warn "Red light ahead")
+; => "red light ahead"
