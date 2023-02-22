974db0a257646d1e09f1761d2bbb8bc89e20ce51
carreraprogrammer <carreraprogrammer@gmail.com>
2023-02-22 07:28:03 -0500

    Unify data example

--- src/chapter-two.clj
+++ src/chapter-two.clj
@@ -4,6 +4,20 @@
 
   ;; AMPS
 
+(def human-consumption   [8.1 7.3 6.6 5.0])
+(def critter-consumption [0.0 0.2 0.3 1.1])
+(defn unify-diet-data
+  [human critter]
+  {:human human
+   :critter critter})
+
+(map unify-diet-data human-consumption critter-consumption)
+
+; => ({:human 8.1, :critter 0.0}
+{:human 7.3, :critter 0.2}
+{:human 6.6, :critter 0.3}
+{:human 5.0, :critter 1.1})
+
 (map titleize ["Hamsters" "Ragnarok"])
 ; => ("Hamsters for the Brave and True" "Ragnarok for the Brave and True")
 
