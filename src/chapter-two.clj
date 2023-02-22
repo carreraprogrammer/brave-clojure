8c14a7056a6844fe32306b7eec25c741aeb53147
carreraprogrammer <carreraprogrammer@gmail.com>
2023-02-22 08:16:22 -0500

    Understand Lazzy seq

--- src/chapter-two.clj
+++ src/chapter-two.clj
@@ -121,3 +121,44 @@
 
 (concat [1 2] [3 4])
 ; => (1 2 3 4)
+
+;; LAZY SEQ EFFICIENCY
+
+(def vampire-database
+  {0 {:makes-blood-puns? false, :has-pulse? true  :name "McFishwich"}
+   1 {:makes-blood-puns? false, :has-pulse? true  :name "McMackson"}
+   2 {:makes-blood-puns? true,  :has-pulse? false :name "Damon Salvatore"}
+   3 {:makes-blood-puns? true,  :has-pulse? true  :name "Mickey Mouse"}})
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))
+       record))
+
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
+
+(time (vampire-related-details 0))
+
+; => "Elapsed time: 1006.3868 msecs"
+;{:makes-blood-puns? false, :has-pulse? true, :name "McFishwich"}
+
+(time (def mapped-details (map vampire-related-details (range 0 1000000))))
+; => "Elapsed time: 0.049 msecs"
+; => #'user/mapped-details
+
+(time (first mapped-details))
+; => "Elapsed time: 32030.767 msecs"
+; => {:name "McFishwich", :makes-blood-puns? false, :has-pulse? true}
+
+(time (identify-vampire (range 0 1000000)))
+;=> "Elapsed time: 32019.912 msecs"
+; => {:name "Damon Salvatore", :makes-blood-puns? true, :has-pulse? false
