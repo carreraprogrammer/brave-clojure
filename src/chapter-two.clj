67603733819406f4e076cc07478e0fc2e0341fe1
carreraprogrammer <carreraprogrammer@gmail.com>
2023-02-22 08:55:15 -0500

    Add Into

--- src/chapter-two.clj
+++ src/chapter-two.clj
@@ -187,3 +187,34 @@
 
 (take 10 (odd-numbers))
 
+; INTO
+
+(map identity {:sunlight-reaction "Glitter!"})
+; => ([:sunlight-reaction "Glitter!"])
+
+(into {} (map identity {:sunlight-reaction "Glitter!"}))
+; => {:sunlight-reaction "Glitter!"}
+
+(map identity [:garlic :sesame-oil :fried-eggs])
+; => (:garlic :sesame-oil :fried-eggs)
+
+(into [] (map identity [:garlic :sesame-oil :fried-eggs]))
+; => [:garlic :sesame-oil :fried-eggs]
+
+(map identity [:garlic-clove :garlic-clove])
+; => (:garlic-clove :garlic-clove)
+
+(into #{} (map identity [:garlic-clove :garlic-clove]))
+; => #{:garlic-clove}
+
+(into {:favorite-emotion "gloomy"} [[:sunlight-reaction "Glitter!"]])
+; => {:favorite-emotion "gloomy" :sunlight-reaction "Glitter!"}
+
+(into ["cherry"] '("pine" "spruce"))
+; => ["cherry" "pine" "spruce"]
+
+(into {:favorite-animal "kitty"} {:least-favorite-smell "dog"
+                                  :relationship-with-teenager "creepy"})
+; => {:favorite-animal "kitty"
+;     :relationship-with-teenager "creepy"
+;     :least-favorite-smell "dog"}
