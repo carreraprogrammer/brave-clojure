8c624eb974eddd5e163f7f47a78509467460bd34
carreraprogrammer <carreraprogrammer@gmail.com>
2023-02-28 12:35:31 -0500

    Add examples

--- src/solutions-CH5.clj
+++ src/solutions-CH5.clj
@@ -103,7 +103,7 @@
 
 ((multiplicar-por-10 5))
 
-;; From the next map, use comp to obtain all the information
+;;1. From the next map, use comp to obtain all the information
 
 (def nested-map {:name "Alice"
                  :age 25
@@ -124,24 +124,24 @@
 
 (def phone (comp :phone :contact))
 
-; Phone
+; 2 Phone
 (get-in nested-map [:contact :phone])                       ;=> "555-1234"
 (phone nested-map)                                          ; => "555-1234"
 
-; Street
+; 3 Street
 (def street (comp :street :address :contact))
 
 (get-in nested-map [:contact :address :street])
 
 (street nested-map)
 
-;; Number
+;;4 Number
 
 (def number (comp :number :address :contact))
 
 (number nested-map)                                         ; =>  123
 
-;; City
+;;5 City
 
 (def city (comp :city :address :contact))
 
@@ -149,20 +149,20 @@
 
 (city nested-map)                                           ;=> => "NY"
 
-;; zip-code
+;;6 zip-code
 (def zip-code (comp :zip-code :address :contact))
 
 (get-in nested-map [:contact :address :zip-code])
 (zip-code nested-map)
 
-;; Given a list of numbers, return a vector with the numbers squared and then sorted in descending order:
+;;7 Given a list of numbers, return a vector with the numbers squared and then sorted in descending order:
 
 (def squared-and-sorted (comp (partial into []) (fn [collection] (map #(Math/pow % 2) collection)) (partial sort <)))
 
 (squared-and-sorted [ 2 4 8 3 1])                           ;; => [1.0 4.0 9.0 16.0 64.0]
 
 
-;; Use partial to create a function that returns the square of an array
+;;8 Use partial to create a function that returns the square of an array
 
 (defn square-array [array]
   (map #(Math/pow % 2) array))
@@ -171,7 +171,7 @@
 
 (square-array-partial [1 2 3 4 5])
 
-;; Given a map that contains employee information, use comp to obtain their email addresses as a list of strings.
+;;9 Given a map that contains employee information, use comp to obtain their email addresses as a list of strings.
 
 (def employee-map {:employees [{:name "Alice" :email "alice@example.com"}
                                {:name "Bob" :email "bob@example.com"}]})
@@ -181,7 +181,7 @@
 (list-of-emails employee-map)                               ;;=>("alice@example.com" "bob@example.com")
 
 
-;; Given a map that contains a person's information, use comp to obtain their full address as a list of strings.
+;;10 Given a map that contains a person's information, use comp to obtain their full address as a list of strings.
 
 (def person-map {:name "Alice"
                  :age 25
@@ -203,3 +203,106 @@
 (def full-address (comp  (partial clojure.string/join " ") from-map-to-string))
 
 (full-address person-map)                                   ; => "Main Street 123 NY 12345"
+
+;; 11 Given a list of strings, use comp and partial to obtain a list of the lengths of the strings sorted in ascending order.
+
+(def strings ["hello" "world" "clojure" "partial" "comp"])
+
+(map #(count %) strings)
+
+(def sort-string-lengths (comp (partial sort <) (partial map #(count %))))
+
+(sort-string-lengths strings)                               ;=> (4 5 5 7 7)
+
+;; 12 Given a list of integers, use comp and partial to obtain the sum of the squares of the even numbers in the list.
+
+(def numbers [1 2 3 4 5 6 7 8 9 10])
+
+;; first filter even numbers
+
+(defn even-numbers [numbers]
+  (vec (filter #(= (rem % 2) 0) numbers)))
+
+(even-numbers numbers)
+
+;; then square each number
+
+(defn square-numbers [numbers]
+  (vec (map #(Math/pow % 2) numbers)))
+
+(square-numbers numbers)
+
+;; then reduce the vector
+(reduce + numbers )
+
+;; Use comp
+
+
+(def square-even-numbers (comp
+                           (partial reduce +)
+                           (partial square-numbers)
+                           (partial even-numbers)))
+
+(square-even-numbers numbers)                               ;; => 220
+
+;; 13 Given a list of maps containing information about students, use comp to obtain the average grade for all the students.
+
+(def students [{:name "Alice" :grade 8}
+               {:name "Bob" :grade 7}
+               {:name "Charlie" :grade 9}])
+
+;; first obtain the grades in an array
+
+(defn grades [students]
+  (vec (map :grade students)))
+
+(grades students)  ; => [8 7 9]
+
+;; Average of grades
+
+(def avg-grades (comp #(/ % 2) (partial reduce +) #(grades %)))
+
+(avg-grades students)                                       ;; => 12
+
+
+;; 14 Given a list of numbers, use comp and partial to obtain a list of the numbers cubed and then filtered to only include the ones that are greater than 100.
+
+(def numbers [1 2 3 4 5 6 7 8 9 10])
+
+(defn cube-numbers [numbers]
+  (map #(Math/pow % 3) numbers))
+
+(cube-numbers numbers)
+
+(defn more-than-100 [numbers]
+  (filter #(> % 100) numbers))
+
+(more-than-100 (cube-numbers numbers))                      ;; => (125.0 216.0 343.0 512.0 729.0 1000.0)
+
+
+;; 15 Given a list of strings, use comp and partial to obtain a list of the strings in reverse order with the first letter capitalized.
+
+(def strings ["hello" "world" "clojure" "partial" "comp"])
+
+;; first, reverse strings
+
+(defn reverse-str [strings]
+  (map #(apply str (reverse %)) strings))
+(reverse-str strings)
+
+(def reverse-capitalize (comp (partial map #(clojure.string/capitalize %)) (partial reverse-str)))
+
+(reverse-capitalize strings)                                ;; =>  ("Olleh" "Dlrow" "Erujolc" "Laitrap" "Pmoc")
+
+
+
+;; 16 Given a list of numbers, use comp and partial to obtain the absolute difference between the maximum and minimum values in the list.
+
+
+(def numbers [1 5 -3 7 2 4])
+
+
+
+
+
+
