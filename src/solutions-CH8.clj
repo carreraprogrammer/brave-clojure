5347c43a6156346024d06cecc0f7434dc0cbb098
carreraprogrammer <carreraprogrammer@gmail.com>
2023-02-28 20:50:04 -0500

    defmacro solutions

--- src/solutions-CH8.clj
+++ src/solutions-CH8.clj
@@ -10,3 +10,18 @@
 
 (my-or nil false 2 1)
 (macroexpand '(my-or nil false 2 1))
+
+;; excersise 3
+
+(defmacro defattrs
+  [& assignments]
+  `(do
+     ~@(map
+         (fn [[retr attr]] `(def ~retr ~attr))
+         (partition 2 assignments))))
+
+(defattrs c-int :intelligence wokring? :should-work)
+
+(print wokring? c-int)
+
+(macroexpand '(defattrs c-int :intelligence test :should-work))
